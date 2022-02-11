68474148b3e1ad2c982ad7aa897511997769935c
rensigondaliya <Rensi.Gondaliya@einfochips.com>
2022-02-11 12:50:27 +0530

    upload

---
+++ C#/Demo27/Demo24_Thread/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo24_Thread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo1.Run();
+
+            Console.WriteLine(" this is a a demo 2");
+            Demo2.Run();  
+        }
+    }
+}
---
+++ C#/Demo27/Demo26/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo1.Run();
+            Demo2.Run();
+        }
+    }
+}
